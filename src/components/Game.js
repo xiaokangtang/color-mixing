a6d26933977c798dd25a6462949277ccedc3f068
Xiaokang Tang <xiaokangt@hotmail.com>
2020-08-04 20:58:05 -0400

    correct working

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,28 @@
 import React, { useState, useEffect } from 'react';
 
+const ColorMixingBtn = props => (
+  <button
+    className="colorBtn"
+    style={{ backgroundColor: props.colorMixing }}
+    onClick={() => props.onClick(props.colorMixing)}
+  >
+  </button>
+);
+
+const MixedColor = props => (
+  <div
+    className = 'mixedColor'
+    style={{ backgroundColor: props.mixedColor}} 
+  >
+  </div>
+)
+
 const colors = {
   'orange': ['red', 'yellow'],
   'grey': ['black', 'white'],
-  'purple': ['red', 'blue']
+  'purple': ['red', 'blue'],
+  'green': ['yellow', 'blue'],
+  'brown': ['red', 'green']
 };
 
 const colorsMixedArr = Object.keys(colors);
@@ -14,29 +33,65 @@ colorsMixedArr.forEach((key) => {
 });
 colorMixingArr = Array.from(new Set(colorMixingArr));
 
-const ColorMixingBtn = props => (
-  <button
-    className="colorBtn"
-    style={{ backgroundColor: props.colorMixing }}
-  >
-    {props.colorMixing}
-  </button>
-);
+const startGame = () => {
+  const ind = Math.floor(Math.random() * colorsMixedArr.length);
+  const mixedColor = colorsMixedArr[ind];
+  const correctAns = colors[mixedColor];
+  const [colorLeft, setColorLeft] = useState(mixedColor);
+  const [status, setStatus] = useState('');
+  const [chosenColors, setChosenColors] = useState([]);
+
+  const mixCheck = (chosenColors) => {
+    if (correctAns.sort().join() === chosenColors.sort().join()) {
+      const newColorLeft = colorsMixedArr[Math.floor(Math.random() * colorsMixedArr.length)];
+      setStatus('correct');
+      setChosenColors([]);
+      setColorLeft(newColorLeft);
+    } else {
+      setStatus('incorrect');
+    }
+  }
+  return { mixedColor, status, chosenColors, mixCheck };
+}
 
 const Game = () => {
-  return (
-    <div className="body">
-      <h1>Color Mixing Game</h1>
-      <div className="left">
+  const {
+    mixedColor,
+    status,
+    chosenColors,
+    mixCheck,
+  } = startGame();
 
+  const onBtnClick = (chosenColor) => {
+    chosenColors.push(chosenColor);
+    if (chosenColors.length >= 2) {
+      mixCheck(chosenColors);
+    }
+  }
+  return (
+    <div>
+      <h1>Color Mixing Game</h1>
+      <div className="body">
+        
+        <div className="left">
+          <MixedColor mixedColor={mixedColor} />
+        </div>
+        <div className="right">
+          {colorMixingArr.map((colorMixing, index) => (
+            <ColorMixingBtn
+              key={index}
+              colorMixing={colorMixing}
+              onClick={onBtnClick}
+            />
+          ))}
+        </div>
       </div>
-      <div className="right">
-        {colorMixingArr.map((colorMixing, index) => (
-          <ColorMixingBtn
-            key={index}
-            colorMixing={colorMixing}
-          />
-        ))}
+      <div>
+        {status === 'correct'
+          ? (<p>Correct!</p>)
+          : status === 'incorrect'
+            ? (<p>Incorrect!</p>)
+            : (<p>Please select</p>)}
       </div>
     </div>
   );
