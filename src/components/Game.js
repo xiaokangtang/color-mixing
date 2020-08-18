bcb539b47b8748d4d0fa13031c607412b791a54b
Xiaokang Tang <xiaokangt@hotmail.com>
2020-08-18 19:29:06 -0400

    add timer

--- src/components/Game.js
+++ src/components/Game.js
@@ -23,6 +23,33 @@ const PlayAgain = props => (
   </div>
 );
 
+const Timer = props => {
+  const [seconds, setSeconds] = useState(0);
+  useEffect(() => {
+    let interval = null;
+    if (props.gameStatus !== 'complete') {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [seconds, props.gameStatus]);
+
+  return (
+    props.gameStatus === 'complete' ? (
+      <div>
+        <p>You used {seconds}s</p>
+      </div>
+    ) : (
+      <div>
+        <p>{seconds}</p>
+      </div>
+    )
+  )
+}
+
 const colors = {
   'orange': ['red', 'yellow'],
   'grey': ['black', 'white'],
@@ -119,6 +146,7 @@ const Game = props => {
               ? (<p>Incorrect!</p>)
               : (<p>Please select</p>)}
       </div>
+      <Timer gameStatus={gameStatus} />
     </div>
   );
 };
