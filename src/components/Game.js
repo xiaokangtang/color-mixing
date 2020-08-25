111c032a19fa27bb854a72d5bbf046c31924d0be
Xiaokang Tang <xiaokangt@hotmail.com>
2020-08-24 21:12:59 -0400

    minor styling

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,13 +1,29 @@
 import React, { useState, useEffect } from 'react';
+import { createUseStyles } from 'react-jss';
 
-const ColorMixingBtn = props => (
-  <button
-    className="colorBtn"
-    style={{ backgroundColor: props.colorMixing }}
+const useStyles = createUseStyles({
+  myBtn: {
+    border: [
+      ['thin', 'solid', '#ddd']
+    ],
+    width: 45,
+    height: 45,
+    margin: 10,
+    'font-size': '25px',
+    backgroundColor: props => props.colorMixing,
+  }
+});
+
+const ColorMixingBtn = props => {
+  const classes = useStyles(props);
+  return(
+    <button
+    className= {classes.myBtn}
     onClick={() => props.onClick(props.colorMixing)}
   >
   </button>
-);
+  )
+};
 
 const MixedColor = props => (
   <div
