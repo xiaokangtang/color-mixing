3b8c2c681114861b3da5267269fb1a77a8c6f1c5
Xiaokang Tang <xiaokangt@hotmail.com>
2020-12-21 14:20:39 -0500

    ay11

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,11 +15,12 @@ module.exports = {
     },
     sourceType: 'module',
   },
-  plugins: ['react', 'react-hooks'],
+  plugins: ['react', 'react-hooks', "jsx-a11y"],
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    "plugin:jsx-a11y/recommended",
   ],
   settings: {
     react: {
